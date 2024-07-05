3255294a7faa231f5715ecca4830a44e520b4eca
zaerbib <ilunga.joel@yahoo.fr>
2024-07-05 19:52:20 +0200

    update develop

--- restful_gpt_transformer.py
+++ restful_gpt_transformer.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import pipeline, MarianMTModel, MarianTokenizer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load the pre-trained model and tokenizer
 model_name = 'Helsinki-NLP/opus-mt-fr-en'
