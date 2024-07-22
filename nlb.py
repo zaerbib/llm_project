d0b7f32a0382f28664be86b0b15c47f437740601
zaerbib <ilunga.joel@yahoo.fr>
2024-07-22 23:19:01 +0200

    add cache structure

--- nlb.py
+++ nlb.py
@@ -3,6 +3,18 @@ from flask_cors import CORS
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 import torch
 
+## define cache structure
+class Cache: 
+    def __init__(self):
+        self.cache = {}
+    
+    def get(self, key): 
+        return self.cache.get(key)
+    
+    def set(self, key, value):
+        self.cache[key] = value
+        
+
 app = Flask(__name__)
 CORS(app)
 
@@ -10,6 +22,9 @@ CORS(app)
 huggingface_model = "facebook/nllb-200-distilled-600M"
 
 device = torch.device("cpu")
+
+# cache
+cache = Cache()
 
 # Load the model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(huggingface_model)
@@ -21,6 +36,7 @@ eng_to_fra_pipeline = pipeline('translation', model=model, tokenizer=tokenizer, 
 fra_to_eng_pipeline = pipeline('translation', model=model, tokenizer=tokenizer, src_lang='fra_Latn', tgt_lang='eng_Latn')
 alb_to_eng_pipeline = pipeline('translation', model=model, tokenizer=tokenizer, src_lang='als_Latn', tgt_lang='eng_Latn')
 eng_to_alb_pipeline = pipeline('translation', model=model, tokenizer=tokenizer, src_lang='eng_Latn', tgt_lang='als_Latn')
+fra_to_alb_pipeline = pipeline('translation', model=model, tokenizer=tokenizer, src_lang='fra_Latn', tgt_lang='als_Latn')
 
 @app.route('/')
 def index():
@@ -32,15 +48,28 @@ def predict():
     data = request.json
     input_text = data['input']
     direction = data.get('direction', 'eng_to_fra')  # Default to English to French
+    
+    key = (input_text, direction)
 
+    if cache.get(key) is not None:
+        output = cache.get(key)
+        return jsonify({"translation": output})
+    
     if direction == 'eng_to_fra':
         result = eng_to_fra_pipeline(input_text, max_length=1000, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        cache.set(key, result[0]['translation_text'])
     elif direction == 'fra_to_eng':
         result = fra_to_eng_pipeline(input_text, max_length=1000, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        cache.set(key, result[0]['translation_text'])
     elif direction == 'eng_to_alb':
         result = eng_to_alb_pipeline(input_text, max_length=1000, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        cache.set(key, result[0]['translation_text'])
     elif direction == 'alb_to_eng':
         result = alb_to_eng_pipeline(input_text, max_length=1000, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        cache.set(key, result[0]['translation_text'])
+    elif direction == 'fra_to_alb':
+        result = fra_to_alb_pipeline(input_text, max_length=1000, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        cache.set(key, result[0]['translation_text'])
     else:
         return jsonify({"error": "Invalid translation direction"}), 400
 
